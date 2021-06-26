ab8427bf1b3d7034fc43debf9bbca05a2226c521
Pavithrap1195 <pavithrapxworkz@gmail.com>
2021-06-26 11:41:02 +0530

    Using Variables and datatypes program is coded

---
+++ Bottle.java
@@ -0,0 +1,16 @@
+class Bottle{
+
+public static void main(String a[])
+{
+String companyName ="milton";
+String type ="stainless steel";
+String capacity ="1000 ML";
+String color ="silver";
+
+System.out.println("Bottle is made up of many materials");
+System.out.println(companyName);
+System.out.println(type);
+System.out.println(capacity);
+System.out.println(color);
+}
+}
---
+++ Mobile.java
@@ -0,0 +1,18 @@
+class Mobile{
+
+public static void main(String a[])
+{
+String mobileName = "redmi note 5 pro";
+String androidVersion ="9 pkq1.180904.001";
+String storage ="64.00gb";
+String modelNumber ="MEI7S";
+ 	
+System.out.println("Mobile has both pros and cons");
+System.out.println(mobileName);
+System.out.println(androidVersion);
+System.out.println(storage);
+System.out.println(modelNumber);
+}
+
+
+}
